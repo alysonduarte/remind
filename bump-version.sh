aee928c1195a6ef7fb341e9ade694338341cb31a
Alyson Duarte <capivara@bull-dolser.(none)>
2013-04-15 10:37:48 -0300

    Add bump version.sh and add remove Recurring function

---
+++ bump-version.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+GITROOT=$(git rev-parse --show-toplevel)
+
+usage() {
+	echo "usage: $0 <version>"
+}
+
+Version=$1
+
+if [ -z $Version ]; then
+	usage
+	exit 1
+fi
+
+cd "$GITROOT"
+git ls-files "*.vim" | while read File; do
+	if ! grep -Eq "^\" Version: v.+$" "$File"; then
+		continue
+	fi
+
+	echo "Bumping version number in $File..."
+	if ! sed "s/^\(\" Version: \)v.*$/\1v$Version/" "$File" > "$File~"; then
+		echo "Could not replace Version in $File." >&2
+		exit 2
+	fi
+	mv "$File~" "$File"
+	git add "$File"
+done
+
+echo "Now run: git commit -a -m \"Bumped version to v$Version\""
